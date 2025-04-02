4015b5626c83e08c28b737be883b7adc1619f274
Philipp Bauer <bauer.philipp96@t-online.de>
2025-04-02 14:08:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,107 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Philipp Bauer</title>
 
     <link rel="stylesheet" href="main_style.css">
-    <link rel="stylesheet" href="resources/public/css/klassen.css">
-    <link rel="stylesheet" href="resources/public/css/mobile.css">
-    <link rel="stylesheet" href="resources/public/css/cookiebanner.css">
-    <link rel="stylesheet" href="resources/public/css/darkmode_button.css">
-    <link rel="stylesheet" href="resources/public/css/darkmode.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="icon" href="resources/public/imgs/favicon.ico" type="image/x-icon">
 </head>
 
 <body>
-    <!-- Cookie Banner -->
-    <div id="cookie_banner">
-        <div>
-            <span>Diese Website verwendet Cookies. Bitte akzeptiere oder lehne ab.</span> <br>        
-            <button onclick="acceptCookies()" class="btn btn-success">Akzeptieren</button>
-            <button class="btn btn-danger" onclick="rejectCookies()">Ablehnen</button>
-        </div>
-    </div>
-
     <section id="sidebar">
-        <div id="sidebar_header" class="ml-2">
+        <div id="sidebar_header" class="pl-2">
             <a href="" title="Startseite" class="no_decoration">
                 <h1 id="txt_name" class="darkmode_text mt-5">Philipp Bauer</h1>
                 <h3 id="txt_titel" class="darkmode_text mb-4 fs-5">Fachinformatiker für Anwendungsentwicklung</h3>
             </a>        
         </div>
+    </section>  
 
-        <div id="sidebar_menu">
-            <a href="" id="btn_menu1" class="no_decoration mb-1" title="Menu 1">Menu 1</a>
-            <a href="resources/public/views/menu2.php" id="btn_menu2"class="no_decoration" title="Menu 7">Menu 2</a>
-        </div>
-
-        <div id="sidebar_footer">
-            <div id="kontakt">
-                <a href="resources/public/views/kontakt.php" title="Kontakt" id="link_kontakt" class="no_decoration darkmode_teext"><p>Kontakt</p></a>
-            </div>
-
-            <div id="footer_links">
-                <label class="switch mb-2">
-                    <p class="darkmode_text">Dunkler Modus</p>	
-                    <input id="darkmode-toggle" type="checkbox">
-                    <span></span>  
-                </label>
-            </div>
-        </div>
-    </section>
-
-    <section id="section_main">
-        <div id="main_container">
-            <p>Menu 1</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio animi corrupti cupiditate in inventore blanditiis esse maxime, ipsum, fugit hic accusantium porro accusamus tempore necessitatibus similique minima explicabo. Voluptatibus doloremque esse necessitatibus est mollitia placeat possimus minus culpa dolorum adipisci optio debitis ad, ut, beatae, quo porro corrupti inventore distinctio minima dicta perferendis magnam. Quasi, rerum libero velit facilis unde, assumenda exercitationem aspernatur nostrum cupiditate expedita necessitatibus, fugit doloribus placeat consequuntur? Repellat ipsam quae debitis architecto aspernatur doloribus. Temporibus officiis animi optio impedit incidunt laboriosam aut veritatis vel magnam ex natus harum vitae, in officia non architecto. Deleniti aliquam possimus dolorem quia unde placeat ut aut repudiandae dignissimos, soluta aspernatur molestiae, harum sunt, id atque cumque vel voluptate similique blanditiis iure quae quos. Nulla saepe tempora illo sequi neque, maxime atque aliquid praesentium enim ipsa odio culpa voluptas nesciunt molestiae eaque adipisci cumque sint facilis dolores dolor, quis magni esse omnis? Aliquid ut, est dolorum veritatis velit maxime aspernatur id voluptatem harum itaque laborum consequatur unde dolorem impedit saepe eveniet modi? Eius consequuntur eveniet suscipit repellendus quam esse. Accusantium magnam quia nulla in, consequuntur doloremque officiis, aliquam est rerum sequi ab velit labore repellat debitis pariatur amet dolores nobis dolorum. Minus, vero molestiae eveniet ducimus perferendis debitis. Sit quaerat quia sunt saepe, reiciendis rerum odio animi consectetur? Possimus dicta accusantium repellendus ab doloribus minus consequuntur est illum! Minima quidem minus earum dolore esse ipsum suscipit animi impedit, sit tempore iusto est ducimus recusandae excepturi natus incidunt harum aliquam totam temporibus. Nisi, corrupti similique quod deleniti repellendus, molestias omnis sequi nesciunt cum fuga itaque, perspiciatis magni facilis nemo quaerat! Qui assumenda tempore quam quae perspiciatis neque aliquid, impedit voluptatum explicabo corporis esse maxime nulla, excepturi non tempora consectetur magnam necessitatibus quas! Eos nemo esse in sequi labore error porro. Quod explicabo officia praesentium dolorem, amet doloremque cumque. Sunt ea ex fuga expedita repudiandae! Sequi deserunt ab excepturi id aliquam. Tempore in facilis quasi fugit eum reiciendis ipsa ipsam minima voluptatem provident officiis obcaecati necessitatibus aliquid consequuntur, dolor, odio repudiandae corporis dolores ducimus sed soluta deserunt dicta culpa sit? Alias ex quam ducimus cum praesentium, reprehenderit exercitationem officiis corporis sapiente suscipit perferendis debitis similique recusandae commodi molestiae quidem culpa ipsum. Dolore, enim aliquid consectetur esse quibusdam beatae tenetur officia voluptatibus magni commodi nostrum minima magnam ad porro. Totam quisquam obcaecati voluptates? Culpa nisi vitae modi ratione facilis maiores quas, qui velit vero perferendis veritatis dolorum, libero voluptas sunt, odio voluptate totam! Reprehenderit consequuntur eius quidem vero laboriosam dolore sunt fugit officia velit molestiae placeat, ducimus cum quod, earum commodi at harum provident tenetur. Quisquam, at sint, alias sapiente ipsam vel facilis accusamus sit laboriosam quam quia accusantium hic rem numquam, molestias placeat eum veniam dolorum omnis! Quas possimus sint explicabo eligendi eaque id temporibus nihil, earum esse corporis deleniti facere, autem aperiam sequi voluptatum quasi. Pariatur aut id tempore, ipsa fugiat esse exercitationem sunt eaque saepe. Sint provident reprehenderit excepturi et voluptas tempora neque totam earum omnis a ratione libero, itaque aut.</p>
-            <!-- ToDos anzeigen -->
-            <?php
-                            // $servername = "mysql11.manitu.net";
-                            // $username = "u116323";
-                            // $password = "1eAMxewA35y2JPdK";
-                            // $database = "db116323";
-
-                            // // $username = "root";
-                            // // $password = "1234";
-                            // // $database = "philipp-bauer";
-
-                            // // Creating a connection
-                            // $conn = new mysqli($servername, $username, $password, $database);
-
-                            // // Checking the connection
-                            // if ($conn->connect_error) {
-                            //     die("Connection failed: " . $conn->connect_error);
-                            // }
-
-                            // // SQL-Abfrage vorbereiten
-                            // $sql = "SELECT * FROM todos";
-                            // $result = $conn->query($sql);                
-                                
-                            // // Daten ausgeben, falls vorhanden
-                            // if ($result->num_rows > 0) {
-                            //     while ($row = $result->fetch_assoc()) {
-                                
-                            //         echo "<div style='border: 1px solid black; padding: 5px; margin-top: 10px; background: rgb(202,215,215); background: linear-gradient(170deg, rgba(202,215,215,1) 0%, rgba(233,240,240,1) 99%)'>
-                            //                 <td><b style='font-size: 25px' title='{$row['titel']}'>{$row['titel']}</b></td> <br>
-                            //                 <td title='{$row['text']}'>{$row['text']}</b></a></td>
-                            //             </div>";
-                            //     }                            
-                            // }
-                            // else {
-                            //     echo "<tr>
-                            //             <td colspan='4'>Keine ToDos gefunden</td>
-                            //         </tr>";
-                            // }
-                            
-                            // // Verbindung schließen
-                            // $conn->close();
-            ?>
-        </div>
-    </section>    
-
-    <script src="resources/private/js/cookiebanner.js"></script>
-    <script src="resources/private/js/darkmode.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
